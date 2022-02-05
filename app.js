c93901f6fb281c7d9613fa53dd4cd8eadcd9b58f
Đinh Xuân Linh <65463178+xulidinh88@users.noreply.github.com>
2022-02-06 02:58:17 +0700

    Update app.js

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ const oneSecond = 1000;
 const deadline = document.querySelector(".deadline");
 const items = document.querySelectorAll(".deadline-format h4");
 
-let futureDate = new Date(2021, 12, 1, 0, 00, 0);
+let futureDate = new Date(2022, 12, 1, 0, 00, 0);
 const futureTime = futureDate.getTime();
 
 function getRemainingTime() {
